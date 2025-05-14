4759c12a36c0da041a3c58a28a1cef3385a2690c
ayanasgba <ayana12q34@gmail.com>
2025-05-14 13:08:23 +0200

    first commit

---
+++ src/main/java/com/example/autowerkstatt/autowerkstatt/AutowerkstattApplication.java
@@ -0,0 +1,13 @@
+package com.example.autowerkstatt.autowerkstatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowerkstattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutowerkstattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/autowerkstatt/autowerkstatt/AutowerkstattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autowerkstatt.autowerkstatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutowerkstattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
